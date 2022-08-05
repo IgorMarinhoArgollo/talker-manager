aefb7a1d9096b1230a77a52912c010a83d2244a3
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-08-05 13:26:57 -0300

    feat: starting project configuration
    creating index.js, app.js using express, helmet, rate-limit, express.json and errorMiddleware on
    app.js, also creating error middleware and dotenv with PORT and using it on index.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const helmet = require('helmet');
+const limiter = require('./helpers/limiter');
+
+const app = express();
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+
+
+app.use(error);
+
+module.exports = app;
---
+++ middlewares/error.js
@@ -0,0 +1,8 @@
+const errorMiddleware = (err, _req, res, _next) => {
+  if (err.code && err.status) {
+    return res.status(err.status).json({ message: err.message })
+  }
+  return res.status(500).json({ message: err.message || 'Internal Server Error' });
+};
+
+module.exports = errorMiddleware;
